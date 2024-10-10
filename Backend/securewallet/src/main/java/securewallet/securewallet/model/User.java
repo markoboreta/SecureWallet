a6e137a476c2fd2be03521bcf9655a238c12c815
Marko Boreta <markoboreta61@gmail.com>
2024-10-10 19:44:03 +0200

    Set User class

--- Backend/securewallet/src/main/java/securewallet/securewallet/model/User.java
+++ Backend/securewallet/src/main/java/securewallet/securewallet/model/User.java
@@ -1,9 +1,39 @@
 package securewallet.securewallet.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
 
-    
+    private Long id;
+
+
+    private String username;
+    private String password;
+    private String email;
+    // Getters and setters
+    public Long  getId()
+    {
+        return id;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
 }
